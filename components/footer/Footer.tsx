f32db63faeef81160fdbc4b8533bcd674ad42ad3
Salcedo, Rafael <Rafael.Salcedo@McKesson.com>
2023-01-23 15:55:13 -0500

    Style footer

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,5 +1,19 @@
+import styled from 'styled-components'
+
+const FooterDiv = styled.div`
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+background-color: #DDD;
+color: white;
+padding: 10px;
+text-align: center;
+vertical-align: middle;
+`
+
 function Footer() {
-    return <div><p>Footer</p></div>
+    return <FooterDiv>Footer</FooterDiv>
 }
 
 export default Footer;
