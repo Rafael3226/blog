6ff098c79bf700714c2f87cc5ee68cc2b2b174e4
Salcedo, Rafael <Rafael.Salcedo@McKesson.com>
2023-03-01 16:44:13 -0500

    Page Head Component

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,28 +1,12 @@
 import { ReactElement } from "react";
 import Navbar from "./navbar";
 import Footer from "./footer";
-import Head from "next/head";
+import PageHead from "./head";
 
 function Layout({ children }: { children: ReactElement }) {
   return (
     <>
-      <Head>
-        <title>Rafael Uribe - Software Engineer</title>
-        <meta name="description" content="Technology Blog." />
-        <link rel="icon" href="/images/favicon.ico" />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/images/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/images/favicon-16x16.png"
-        />
-      </Head>
+      <PageHead />
       <Navbar />
       {children}
       <Footer />
---
+++ components/layout/head/PageHead.tsx
@@ -0,0 +1,25 @@
+import Head from "next/head";
+
+function PageHead() {
+  return (
+    <Head>
+      <title>Rafael Uribe - Software Engineer</title>
+      <meta name="description" content="Technology Blog." />
+      <link rel="icon" href="/images/favicon.ico" />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/images/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/images/favicon-16x16.png"
+      />
+    </Head>
+  );
+}
+
+export default PageHead;
