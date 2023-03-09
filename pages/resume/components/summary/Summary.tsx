50143d0e31b9c51085e10a4bfd21e64ed4f6776e
Rafael Uribe <Rafael.Uribe.2080@gmail.com>
2023-03-08 19:01:09 -0500

    Resume changes

--- pages/resume/components/summary/Summary.tsx
+++ pages/resume/components/summary/Summary.tsx
@@ -1,17 +1,25 @@
+import Bold from "components/styled/bold";
 import P from "components/styled/p";
+import { Column, TwoColumns } from "pages/home/TwoColumns";
 
 export default function Summary() {
   return (
-    <section>
+    <>
       <h2>Summary</h2>
-      <P>
-        I have extensive experience as a software engineer, with a focus on
-        implementing CI/CD and using agile methodologies like SCRUM and DevOps.
-        I am highly passionate about both hardware and open-source software, and
-        I am always looking for new and exciting projects to work on. I believe
-        that my combination of technical skills and creative problem-solving
-        abilities make me an asset to any development team.
-      </P>
-    </section>
+      <TwoColumns>
+        <Column></Column>
+        <Column>
+          <P>
+            I have extensive experience as a software engineer, with a focus on
+            implementing <Bold>CI/CD</Bold> and using agile methodologies like
+            <Bold> SCRUM</Bold> and <Bold>DevOps</Bold> . I am highly passionate
+            about both hardware and open-source software, and I am always
+            looking for new and exciting projects to work on. I believe that my
+            combination of technical skills and creative problem-solving
+            abilities make me an asset to any development team.
+          </P>
+        </Column>
+      </TwoColumns>
+    </>
   );
 }
