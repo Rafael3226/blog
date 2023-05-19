6a4e8dada7a72d87121ca6a165f5eb1d3cac4c3f
Rafael Uribe <Rafael.Uribe.2080@gmail.com>
2023-05-18 23:43:25 -0400

    Add image generation project

--- pages/portfolio/Portfolio.tsx
+++ pages/portfolio/Portfolio.tsx
@@ -10,6 +10,13 @@ const projects: ProjectTemplateProps[] = [
     url: "https://638e79130bb609247f32cd73--loquacious-axolotl-441ac4.netlify.app/",
   },
   {
+    title: "Image Generation",
+    description: "",
+    img: "/gen.png",
+    github: "https://github.com/Rafael3226/dall-e-client ",
+    url: "https://elaborate-pavlova-4b9370.netlify.app/",
+  },
+  {
     title: "Social Network",
     description: "",
     img: "/sn.png",
